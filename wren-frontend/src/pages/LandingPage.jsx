48c4c5f0a56f2d70ebd60bc4cddcd1bad07d7fdc
NoriesDev <noriesdevelopment@gmail.com>
2023-12-20 17:15:12 +0100

    commiting frontend

---
+++ wren-frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,24 @@
+
+import About from '../Components/About'
+import RingCard from '../Components/RingCard'
+import VideoHeader from '../Components/VideoHeader'
+import RingHeadline from '../Components/RingHeadline';
+
+
+
+function LandingPage() {
+
+
+  return (
+    <>
+<VideoHeader />
+<About/>
+
+<RingHeadline />
+<RingCard />
+
+    </>
+  )
+}
+
+export default LandingPage
